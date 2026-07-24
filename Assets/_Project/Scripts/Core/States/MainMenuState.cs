8c13b9e719b8cf5e298d54d72399761d488eeff1
Danila1801 <d.popusoi@student.vu.nl>
2026-07-24 14:23:26 +0200

    Fix: Add missing 'using UnityEngine.UI;' to state files (RunFailedState, RunVictoryStates)

--- Assets/_Project/Scripts/Core/States/MainMenuState.cs
+++ Assets/_Project/Scripts/Core/States/MainMenuState.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using OblastZero.Data;
 
 namespace OblastZero.Core.States
 {
--- Assets/_Project/Scripts/Core/States/RunFailedState.cs
+++ Assets/_Project/Scripts/Core/States/RunFailedState.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using OblastZero.Gameplay;
 
 namespace OblastZero.Core.States
